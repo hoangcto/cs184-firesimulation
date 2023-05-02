d332cc59426fe8c5ca83226a006faece74ad52ef
Hoang To <hoangto.losangeles@gmail.com>
2023-05-02 16:15:54 -0700

    fixed pathing for candle

--- fire_sim/js/main.js
+++ fire_sim/js/main.js
@@ -244,13 +244,13 @@ function init() {
 
 
     var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setTexturePath('/obj/');
-    mtlLoader.setPath('/obj/');
+    mtlLoader.setTexturePath('/fire_sim/obj/');
+    mtlLoader.setPath('/fire_sim/obj/');
     mtlLoader.load('CandleStick2.mtl', function (materials) {
         materials.preload();
 
         var objLoader = new THREE.OBJLoader();
-        objLoader.setPath('/obj/');
+        objLoader.setPath('/fire_sim/obj/');
         objLoader.load('CandleStick2.obj', function (object) {
 
         scene.add(object);
